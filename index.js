637e56ac7f9ed5b88ac72b3f6f4d2973f5ac4826
pietro385 <4553765127@estudante.cedupguaramirim.com.br>
2025-08-13 10:20:29 -0300

    oi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+//oi
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
